2b7b2e60d494dfcdabc78d6087d5c035f60fe322
RBC4029 <Rihabboutouila@gmail.com>
2026-06-30 18:01:21 +0200

    Delete atlas_github_v0_1/PROJET ATLAS/atlas_github_v0_1 directory

--- atlas_github_v0_1/PROJET ATLAS/atlas_github_v0_1/backend/app/api/rules.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from app.brain.decision_engine import DEFAULT_RULES
-
-router = APIRouter(prefix="/rules", tags=["rules"])
-
-@router.get("")
-def list_rules():
-    return DEFAULT_RULES
